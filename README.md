5d7ba14b532149a944d4650be97cb6ee417bc3e9
jimboqu <jamesquilter308@gmail.com>
2016-07-25 22:34:52 +0100

    rectangular room

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Taken from Exercises for Programmers - 57 skills
 
+*Rectangular Room
+Work out the area of a room, keeping calculations separate fro the output and use a constant to hold the conversion factor. 
---
+++ rectangular_room.rb
@@ -0,0 +1,27 @@
+CONVERT = 0.09290304
+
+def feet(a, b)
+
+  return a * a + b * b
+
+end
+
+
+def metres(length, width)
+
+  return feet(length, width) * CONVERT
+
+end
+
+
+
+
+
+puts "What is the length of the room in feet?"
+length = gets.chomp.to_i
+puts "What is the width of the room in feet?"
+width = gets.chomp.to_i
+
+puts "The room is #{feet(length, width)} sq feet"
+puts "The room is #{metres(length, width).round(2)} sq metres"
+
