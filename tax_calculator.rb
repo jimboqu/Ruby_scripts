1608da0b1d563db67d18e71949e52d40299036b4
jimboqu <jamesm.quilter@gmail.com>
2018-06-15 14:13:30 +0100

    tax calc

---
+++ tax_calculator.rb
@@ -0,0 +1,22 @@
+puts "What is the order amount?"
+order = gets.chomp.to_i
+puts "What is the state?"
+state = gets.chomp.upcase
+
+def calc (order)
+  extra = order / 5.5 
+  order += extra
+  return order
+end
+
+def tax(state, order)
+  if state == "MW" 
+  	pay = calc(order)
+  	return pay.round(2)
+  else
+  	return order
+  end
+end
+
+print "This total is $#{tax(state, order)}"
+
