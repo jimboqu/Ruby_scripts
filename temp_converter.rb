257b9691719f9402ce9f917d5cbe11e740dd8c34
jimboqu <jamesquilter308@gmail.com>
2018-06-22 21:19:30 +0100

    temp converter

---
+++ temp_converter.rb
@@ -0,0 +1,22 @@
+puts "Press C to convert from celsius to fahrenheit and F to convert from fahrenheit to celsius"
+option = gets.chomp.upcase
+puts "Please enter the temperature"
+temp = gets.chomp.to_i
+
+def fc(option)
+  if option == "C"
+  	return "celsius"
+  else
+  	return "fahrenheit"
+  end
+end
+
+def conTemp(temp, option)
+  if option == "C"
+    return (temp * 9/5) + 32
+  else
+    return (temp - 32) * 5/9
+  end
+end
+
+puts "The temperature in #{fc(option)} is #{conTemp(temp, option)}"
