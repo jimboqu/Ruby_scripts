317e8da1e2ba5cb2651f05d378e21dc3af613a55
jimboqu <jamesquilter308@gmail.com>
2018-11-16 18:57:45 +0000

    password validator

--- password_validator.rb
+++ password_validator.rb
@@ -1,9 +1,13 @@
+#Checks how strong your password is
+
 
 def password_validator(password)
-  if password =~ /[0-9]/ && password.length < 9
+  if password =~ /[0-9][^a-z]/ && password.length < 9
   	return "very weak"
   elsif password =~ /[a-z]/ && password.length < 9 
   	return "weak"
+  elsif password =~ /[a-z0-9\W]/ && password =~ /[\W]/ && password.length >= 9
+  	return "very strong"
   else 
   	return "strong"
   end
